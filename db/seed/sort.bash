3fee937d4010d62e352182250684a2c26cd3b6ed
dhernando <david.hernando91@gmail.com>
2025-01-01 17:29:49 +0100

    remove redundant comments

--- db/seed/sort.bash
+++ db/seed/sort.bash
@@ -2,10 +2,8 @@
 
 set -euo pipefail
 
-# Directory containing the .txt files
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-# Loop through each .txt file in the directory
 for file in "$SCRIPT_DIR"/*.txt; do
   if [[ -f "$file" ]]; then
     echo "Sorting and deduping $file"
