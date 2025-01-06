391fbf69c639fc3909a3b21e7a18aa0f0461694b
dhernando <david.hernando91@gmail.com>
2025-01-06 13:55:54 +0100

    fix callback registration and pass prettier

--- frontend/src/js/entries/app.js
+++ frontend/src/js/entries/app.js
@@ -1,11 +1,12 @@
 import "../../css/app.css";
 import "vite/modulepreload-polyfill";
 import "htmx.org";
-import u from "umbrellajs"
+import u from "umbrellajs";
 import { writeTextToClipboard } from "~/lib/clipboard";
 
 u(document).on("htmx:load", (ev) => {
-  u(el).find(".js-copy").on("click", (ev) => {
+  const el = u(ev.currentTarget);
+  el.find(".js-copy").on("click", (ev) => {
     const target = ev.currentTarget;
     writeTextToClipboard(target.dataset.copyValue);
     const checkmark = u(target).find(".js-checkmark");
@@ -15,11 +16,11 @@ u(document).on("htmx:load", (ev) => {
     }, 2000);
   });
 
-  u(".js-drawer-open").on("click", () => {
-    u("#drawer").removeClass("translate-x-full", "opacity-0")
+  el.find(".js-drawer-open").on("click", () => {
+    u("#drawer").removeClass("translate-x-full", "opacity-0");
   });
 
-  u(".js-drawer-close").on("click", () => {
-    u("#drawer").addClass("translate-x-full")
-  })
+  el.find(".js-drawer-close").on("click", () => {
+    u("#drawer").addClass("translate-x-full");
+  });
 });
