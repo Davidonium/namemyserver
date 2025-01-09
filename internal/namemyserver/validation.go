994a60ddd6a47e30716b4252bf3eedbb37ce3aef
dhernando <david.hernando91@gmail.com>
2025-01-09 09:02:17 +0100

    add validation functions

---
+++ internal/namemyserver/validation.go
@@ -0,0 +1,19 @@
+package namemyserver
+
+import "regexp"
+
+// generated names must be dns subdomain compliant just like kubernetes resources, with the added constraint of them being lowercase
+// validates that all characters are lowercase, alphanumberic and the name must start and end with alphanumeric characters.
+// reference: https://datatracker.ietf.org/doc/html/rfc1123
+var nameRegex = regexp.MustCompile(`^[a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?$`)
+
+var segmentRegex = regexp.MustCompile(`^[a-z0-9]+$`)
+
+
+func ValidateName(name string) bool {
+	return nameRegex.MatchString(name)
+}
+
+func ValidateNameSegment(s string) bool {
+	return segmentRegex.MatchString(s)
+}
---
+++ internal/namemyserver/validation_test.go
@@ -0,0 +1,102 @@
+package namemyserver_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/davidonium/namemyserver/internal/namemyserver"
+)
+
+func TestValidateNameTable(t *testing.T) {
+	cases := []struct {
+		Input string
+		Valid bool
+	}{
+		{
+			Input: "test",
+			Valid: true,
+		},
+		{
+			Input: "teset-with-dashes",
+			Valid: true,
+		},
+		{
+			Input: "test-with-dashes-at-the-end--",
+			Valid: false,
+		},
+		{
+			Input: "---test-with-dashes-at-the-start",
+			Valid: false,
+		},
+		{
+			Input: "UPPERCASE",
+			Valid: false,
+		},
+		{
+			Input: "Test-with-mixed-Case",
+			Valid: false,
+		},
+	}
+
+	for i, tt := range cases {
+		t.Run(fmt.Sprintf("Test Case #%d", i), func(t *testing.T) {
+			ok := namemyserver.ValidateName(tt.Input)
+			if  ok != tt.Valid {
+				t.Errorf("ValidateName() = input: %q - got %v, want %v", tt.Input, ok, tt.Valid)
+			}
+		})
+	}
+}
+
+func TestValidateNameSegmentTable(t *testing.T) {
+	cases := []struct {
+		Input string
+		Valid bool
+	}{
+		{
+			Input: "test",
+			Valid: true,
+		},
+		{
+			Input: "00test",
+			Valid: true,
+		},
+		{
+			Input: "test01",
+			Valid: true,
+		},
+		{
+			Input: "test01test",
+			Valid: true,
+		},
+		{
+			Input: "test-with-dashes",
+			Valid: false,
+		},
+		{
+			Input: "UPPERCASE",
+			Valid: false,
+		},
+		{
+			Input: "TestWithMixedCase",
+			Valid: false,
+		},
+		{
+			Input: "TestWithMixedCaseAndNumbers000",
+			Valid: false,
+		},
+		{
+			Input: "000TestWithMixedCaseAndNumbers",
+			Valid: false,
+		},
+	}
+
+	for i, tt := range cases {
+		t.Run(fmt.Sprintf("Test Case #%d", i), func(t *testing.T) {
+			ok := namemyserver.ValidateNameSegment(tt.Input)
+			if  ok != tt.Valid {
+				t.Errorf("ValidateNameSegment() = input: %q - got %v, want %v", tt.Input, ok, tt.Valid)
+			}
+		})
+	}
+}
