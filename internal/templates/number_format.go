538df4c5b391cb8399a7314376f6bae39f74df4c
dhernando <david.hernando91@gmail.com>
2025-01-20 10:07:40 +0100

    add some comments for me in the future

--- internal/templates/number_format.go
+++ internal/templates/number_format.go
@@ -5,8 +5,12 @@ import (
 	"golang.org/x/text/message"
 )
 
+// p should not be used outside this file, instantiated once to avoid a lot of instantiations.
+// defaulting now to english but the language should change depending on http Accept-Language headers or cookie
+// configuration. Not aiming to support internationalization for now.
 var p = message.NewPrinter(language.English)
 
+// humanInt returns v as a formatted number in the english language
 func humanInt(v int) string {
 	return p.Sprint(v)
 }
